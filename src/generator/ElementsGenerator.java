e91c5d14f79d4bc745dbd56e874ed0e4b5766538
Roman Soldatov <s.roma245@gmail.com>
2019-03-11 21:36:21 +0300

    Added the documentation for generator

--- src/generator/ElementsGenerator.java
+++ src/generator/ElementsGenerator.java
@@ -7,16 +7,22 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 public class ElementsGenerator {
-    private final int STEP_COUNT_ELEM = 100;
-    private final int COUNT_SETS = 90;
+
+    private final int STEP_COUNT_ELEM = 100; // The value of increasing count of elements
+    private final int COUNT_SETS = 90; // Count of sets
     private String pathName;
-    private int number;
+    private int number; // The number of the current set
 
     public ElementsGenerator() {
         pathName = "/Users/smore/Projects/Java/Semester Project/Chan Algorithm/sets/set";
         number = 1;
     }
 
+    /**
+     * Generate sets
+     *
+     * @throws FileNotFoundException
+     */
     public void generate() throws FileNotFoundException {
         for (int i = STEP_COUNT_ELEM; i <= STEP_COUNT_ELEM * COUNT_SETS; i += STEP_COUNT_ELEM) {
             generateSet(i);
@@ -24,13 +30,21 @@ public class ElementsGenerator {
         }
     }
 
+    /**
+     * Generate the set
+     *
+     * @param countElem the count of elements in the set
+     * @throws FileNotFoundException
+     */
     private void generateSet(int countElem) throws FileNotFoundException {
         PrintWriter printWriter = new PrintWriter(new File(pathName + number + ".txt"));
 
         for (int i = 0; i < countElem; i++) {
+            // generate the element's value (-1000; 1000)
             int value = Calculation.getRandomNumber(1000);
             printWriter.print(value + " ");
         }
+
         printWriter.close();
     }
 }
--- src/generator/GenerateElements.java
+++ src/generator/GenerateElements.java
@@ -2,6 +2,9 @@ package generator;
 
 import java.io.FileNotFoundException;
 
+/**
+ * Generates the sets and writes into the files
+ */
 public class GenerateElements {
     public static void main(String[] args) throws FileNotFoundException {
         ElementsGenerator generator = new ElementsGenerator();
